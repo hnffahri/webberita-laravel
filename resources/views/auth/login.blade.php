f35d6570111b1097d373117a8744aa5af25c9a6e
hnffahri <hanifdev09@gmail.com>
2024-07-06 13:41:12 +0700

    ubah html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
             <p>Selamat datang kembali, masukkan kredensial Anda untuk melanjutkan.</p>
           
             <a class="btn btn-dark w-100" href="{{ route('auth.google') }}">
-              <img src="{{ asset('images/logo_google.svg') }}" alt="Logo" width="30"> {{ __('Login Dengan Google') }}
+              <img src="{{ asset('images/logo_google.svg') }}" alt="Logo" width="30"> Login Dengan Google') }}
             </a>
             <div class="divider-text my-4">
               <div class="text">
@@ -29,43 +29,55 @@
             
               <!-- Email Address -->
               <div class="mb-3">
-                  <label for="email" class="form-label">Email</label>
-                  <x-input id="email" name="email" type="email" class="" autocomplete="email" />
+                <label for="email" class="form-label">Email</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                @error('email')
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <!-- Password -->
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <div class="input-group">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
+                <span class="input-group-text">
+                  <i class="fal fa-eye"></i>
+                </span>
               </div>
-            
-          
-              <!-- Password -->
-              <div class="mb-3">
-                  <label for="password" class="form-label">Password</label>
-                  <x-input id="password" name="password" type="password" class="" autocomplete="current-password" />
-              </div>
-            
+                @error('password')
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
               <!-- Remember Me -->
               <div class="mb-3">
                 <div class="row">
                   <div class="col-6">
                     <div class="form-check">
                       <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-                      <label for="remember_me" class="form-check-label">{{ __('Ingat saja saya') }}</label>
+                      <label for="remember_me" class="form-check-label">Ingat saja saya</label>
                     </div>
                   </div>
                   <div class="col-6 text-end">
                     @if (Route::has('password.request'))
                       <a class="text-link" href="{{ route('password.request') }}">
-                        {{ __('Lupa password?') }}
+                        Lupa password?
                       </a>
                     @endif
                   </div>
                 </div>
               </div>
             
-              <button class="btn btn-primary w-100">{{ __('Log in') }}</button>
+              <button class="btn btn-primary w-100">Log in</button>
               <div class="divider-text my-4">
                 <div class="text">
                   Belum punya akun?
                 </div>
               </div>
-              <a class="btn btn-light w-100" href="{{ route('register') }}">{{ __('Daftar') }}</a>
+              <a class="btn btn-light w-100" href="{{ route('register') }}">Daftar</a>
             </form>
           </div>
         </div>
@@ -73,6 +85,33 @@
     </div>
   </div>
 </section>
+
+@push('js')
+    
+<script>
+  const toggleVisibility = (inputId, eyeIcon) => {
+    const inputField = document.getElementById(inputId);
+    const iconElement = eyeIcon.querySelector('i');
+
+    eyeIcon.addEventListener('click', () => {
+      if (inputField.type === 'password') {
+        inputField.type = 'text';
+        iconElement.classList.remove('fa-eye');
+        iconElement.classList.add('fa-eye-slash');
+      } else {
+        inputField.type = 'password';
+        iconElement.classList.remove('fa-eye-slash');
+        iconElement.classList.add('fa-eye');
+      }
+    });
+  };
+
+  const passwordLamaEyeIcon = document.querySelector('#password + .input-group-text');
+
+  toggleVisibility('password', passwordLamaEyeIcon);
+</script>
+
+@endpush
   
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
               <p>Silakan masuk jika Anda sudah membuat akun.</p>
               
               <a class="btn btn-dark w-100" href="{{ route('auth.google') }}">
-                <img src="{{ asset('images/logo_google.svg') }}" alt="Logo" width="30"> {{ __('Daftar Dengan Google') }}
+                <img src="{{ asset('images/logo_google.svg') }}" alt="Logo" width="30"> Daftar Dengan Google
               </a>
               <div class="divider-text my-4">
                 <div class="text">
@@ -26,26 +26,56 @@
           
               <!-- Name -->
               <div class="mb-3">
-                <x-input-label for="name" :value="__('Nama')" />
-                <x-input id="name" name="name" type="text" class="" autocomplete="username" />
+                <label for="name" class="form-label">Nama</label>
+                <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="username">
+                @error('name')
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+                @enderror
               </div>
           
               <!-- Email Address -->
               <div class="mb-3">
-                <x-input-label for="email" :value="__('Email')" />
-                <x-input id="email" name="email" type="email" class="" autocomplete="email" />
+                <label for="email" class="form-label">Email</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                @error('email')
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+                @enderror
               </div>
           
               <!-- Password -->
               <div class="mb-3">
-                <x-input-label for="password" :value="__('Password')" />
-                <x-input id="password" name="password" type="password" class="" autocomplete="new-password" />
+                <label for="password" class="form-label">Password</label>
+                <div class="input-group">
+                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
+                  <span class="input-group-text">
+                    <i class="fal fa-eye"></i>
+                  </span>
+                </div>
+                  @error('password')
+                  <div class="invalid-feedback d-block">
+                      {{ $message }}
+                  </div>
+                  @enderror
               </div>
           
               <!-- Confirm Password -->
               <div class="mb-3">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-                <x-input id="password_confirmation" name="password_confirmation" type="password" class="" autocomplete="new-password" />
+                <label for="konfirmasi_password">Konfirmasi Password</label>
+                <div class="input-group">
+                  <input id="konfirmasi_password" type="password" class="form-control @error('password_konfirmasi') is-invalid @enderror" name="password_confirmation" autocomplete="new-password">
+                  <span class="input-group-text">
+                    <i class="fal fa-eye"></i>
+                  </span>
+                </div>
+                @error('password_konfirmasi')
+                <div class="invalid-feedback d-block">
+                    {{ $message }}
+                </div>
+                @enderror
               </div>
               
               <div class="mb-3">
@@ -57,7 +87,7 @@
                 </div>
                 @enderror
               </div>
-              <button class="btn btn-primary w-100">{{ __('Register') }}</button>
+              <button class="btn btn-primary w-100">Register</button>
               <div class="divider-text my-4">
                 <div class="text">
                   Sudah punya akun?
@@ -65,7 +95,7 @@
               </div>
               <div class="text-center">
                 <a class="btn btn-light w-100" href="{{ route('login') }}">
-                  {{ __('Log In') }}
+                  Log In
                 </a>
               </div>
             </form>
@@ -76,5 +106,31 @@
   </div>
 </section>
   
+@push('js')
+    <script>
+      const toggleVisibility = (inputId, eyeIcon) => {
+        const inputField = document.getElementById(inputId);
+        const iconElement = eyeIcon.querySelector('i');
+    
+        eyeIcon.addEventListener('click', () => {
+          if (inputField.type === 'password') {
+            inputField.type = 'text';
+            iconElement.classList.remove('fa-eye');
+            iconElement.classList.add('fa-eye-slash');
+          } else {
+            inputField.type = 'password';
+            iconElement.classList.remove('fa-eye-slash');
+            iconElement.classList.add('fa-eye');
+          }
+        });
+      };
+    
+      const passwordBaruEyeIcon = document.querySelector('#password + .input-group-text');
+      const konfirmasiPasswordEyeIcon = document.querySelector('#konfirmasi_password + .input-group-text');
+    
+      toggleVisibility('password', passwordBaruEyeIcon);
+      toggleVisibility('konfirmasi_password', konfirmasiPasswordEyeIcon);
+    </script>
+@endpush
 
 @endsection
